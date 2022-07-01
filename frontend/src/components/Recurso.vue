7535b3ca50e5dfb4c4be4f6991d1e9c71f0d029c
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-07-01 18:47:01 -0500

    Frontend a medias

--- frontend/src/components/Recurso.vue
+++ frontend/src/components/Recurso.vue
@@ -1,29 +1,40 @@
 <template>
     <div>
-
+        <h1>Recurso</h1>
     </div>
 </template>
 
 <script>
 export default {
+    name: "Recurso",
     props: {
-        nombre: String,
+        nombre: {
+            type: String,
+            required: true
+        },
     },
     data() {
-        recursoData = {}
+        return {
+            info: ""
+        }
     },
     methods: {
-        getData() {
-            fetch("http://127.0.0.1:5000/" + this.nombre, {
+        async getData() {
+            await fetch("http://127.0.0.1:5000/" + this.nombre, {
                 method: "GET",
                 headers: {
                     "Content-Type" : "application/json",
                 },
             }).then(res => res.json()).then(function (resJson) {
-                this.recursoData = resJson;
                 console.log(resJson);
+                this.info = "hola";
+                console.log(this.info);
+                
             });
         }
+    },
+    mounted() {
+        this.getData();
     }
-}
+};
 </script>
