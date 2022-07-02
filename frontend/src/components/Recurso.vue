338dd4a40d669f448a03b5d32be567daf59337e5
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-07-01 19:07:47 -0500

    se arreglaron cosas

--- frontend/src/components/Recurso.vue
+++ frontend/src/components/Recurso.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
+    <div v-if="recursos">
         <h1>Recurso</h1>
+        <table class="styled-table">
+        <thead>
+            <tr>
+                <td v-for="titulo in recursos[0]">{{ titulo }}</td>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- {% for objeto in objetos %}
+                <tr>
+                    {% for i in range(20) %}
+                    {% if objeto[i] %}
+                    <td>{{ objeto[i] }}</td>
+                    {% endif %}
+                    {% endfor %}
+                </tr>
+            {% endfor %} -->
+        </tbody>
+        </table>
     </div>
 </template>
 
@@ -15,21 +33,18 @@ export default {
     },
     data() {
         return {
-            info: ""
+            recursos: null
         }
     },
     methods: {
-        async getData() {
-            await fetch("http://127.0.0.1:5000/" + this.nombre, {
+        getData() {
+            fetch("http://127.0.0.1:5000/" + this.nombre, {
                 method: "GET",
                 headers: {
                     "Content-Type" : "application/json",
                 },
-            }).then(res => res.json()).then(function (resJson) {
-                console.log(resJson);
-                this.info = "hola";
-                console.log(this.info);
-                
+            }).then(res => res.json()).then(resJson => {
+                this.recursos = resJson;
             });
         }
     },
